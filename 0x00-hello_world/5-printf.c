6baa5864a698ea23e5968f7df9c8b8e5f6e841ef
Ikeji Chiagozie <106752190+Chicajo@users.noreply.github.com>
2022-06-30 18:18:56 +0100

    Create 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/*
+ * File: 5-printf.c
+ * Auth: ikeji chiagozie @ alx-school
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}

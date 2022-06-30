3965a1ab2eff99139ddf2145bd0b888420458701
Ikeji Chiagozie <106752190+Chicajo@users.noreply.github.com>
2022-06-30 18:13:15 +0100

    Create 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+/*
+ * File: 4-puts.c
+ * Auth: chiagozie ikeji @ alx-school.com>
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);

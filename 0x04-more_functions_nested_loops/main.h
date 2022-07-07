a7083d7fd8d1e023891c24e0a7d4a26f99ca4912
Ikeji Chiagozie <106752190+Chicajo@users.noreply.github.com>
2022-07-07 21:14:13 +0100

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+#ifndef main.h
+#define main.h
+
 
 int _putchar(char c);
 int _isupper(int c);
@@ -13,3 +16,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+
+#endif

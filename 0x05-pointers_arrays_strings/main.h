43441104c4803473a426613eff58776611dda3aa
Chicajo <chiagoziesophia@gmail.com>
2022-07-12 13:46:54 -0700

    my first project in array

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- * Auth: ikeji chiagozie @ alx-school
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x04-pointers_arrays_strings directory.
- */
-
 #include <stdlib.h>
 
 int _putchar(char c);

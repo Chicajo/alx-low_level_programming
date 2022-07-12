6e5a7cd8051a3c85353610e065e0cc86e9c28cc6
Chicajo <chiagoziesophia@gmail.com>
2022-07-12 16:19:52 -0700

    modifications to my fourth task

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,27 +6,20 @@
 #include "main.h"
 
 /**
- * rev_string - reverse the string.
+ * print_rev - this function prints a reverse string
+ *
  * @s: The string to be printed.
  */
-void rev_string(char *s)
+void print_rev(char *s)
 {
-	int i, j, k, temp;
+	int index;
 
-	i = 0;
-	while (s[i] != '\0')
+	for (index = 0; s[index] != '\0'; index++)
+		;
+	for (index = index - 1; s[index] != '\0'; index--)
 	{
-		i++;
+		_putchar(s[index]);
 	}
-	k = 0;
-	j = i - 1;
-
-	while (k < j)
-	{
-		temp = s[k];
-		s[k] = s[j];
-		s[j] = temp;
-		k++;
-		j--;
-	}
+	_putchar('\n');
 }
+

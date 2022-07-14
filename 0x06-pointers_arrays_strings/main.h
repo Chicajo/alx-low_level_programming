820a73f5ea9c88bd78866fef881183c152335730
Ikeji Chiagozie <106752190+Chicajo@users.noreply.github.com>
2022-07-14 21:58:01 +0100

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -13,6 +13,7 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *rot13(char *);
 void print_number(int n);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 

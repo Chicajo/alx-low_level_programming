7c5671601c33f4de39a1c2b61c719227641a0a5b
Chicajo <chiagoziesophia@gmail.com>
2022-07-14 18:49:47 -0700

    modification task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,14 +13,16 @@
  *
  * Return: A pointer to the destination string @dest.
  */
-char *strcat(char *dest, const char *src)
+
+char *_strcat(char *dest, char *scr)
 {
+
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
 		dest_len++;
 
-	for (index = 0; src[index]; index++)
+	for (index = 0; src[index] && index < n; index++)
 		dest[dest_len++] = src[index];
 
 	return (dest);

7bd3d96201a67515297d9f07aa91fd4ee578a944
Nwachukwu Michael .C <106752190+Chicajo@users.noreply.github.com>
2022-07-28 18:51:04 +0100

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -13,6 +13,7 @@ int find_len(char *str);
 char *create_xarray(int size);
 char *iterate_zeroes(char *str);
 void get_prod(char *prod, char *mult, int digit, int zeroes);
+int get_digit(char c);
 void add_nums(char *final_prod, char *next_prod, int next_len);
 
 #endif

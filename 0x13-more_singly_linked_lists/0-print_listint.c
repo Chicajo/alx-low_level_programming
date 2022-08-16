c48d2000686dfe38b40af40109adc3d1cb19b290
Chicajo <chiagoziesophia@gmail.com>
2022-08-16 13:59:49 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -14,12 +14,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t n = 0;
-    while (h)
-    {
-        printf("%d\n", h->n);
-        n++;
-        h = h->next;
-    }
-    return (n);
+	size_t n = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		n++;
+		h = h->next;
+	}
+	return (n);
 }
